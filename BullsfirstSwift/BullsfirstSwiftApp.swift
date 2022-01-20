3e71f147ed45589ef335397876a2a3030addc0a1
Naresh Bhatia <naresh.archfirst@gmail.com>
2022-01-20 13:42:51 +0300

    Initial Commit

---
+++ BullsfirstSwift/BullsfirstSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsfirstSwiftApp.swift
+//  BullsfirstSwift
+//
+//  Created by Naresh Bhatia on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BullsfirstSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BullsfirstSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullsfirstSwift
+//
+//  Created by Naresh Bhatia on 1/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
